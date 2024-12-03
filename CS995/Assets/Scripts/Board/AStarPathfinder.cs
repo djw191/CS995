20033ddcf50c6ebae6ca916deff5d3429045a2fe
Derek Woodard <thedjw191@gmail.com>
2024-12-03 13:22:06 -0600

    meta

---
+++ CS995/Assets/Scripts/Board/AStarPathfinder.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Board
+{
+    public class AStarPathfinder
+    {
+        private PathNode[,] _grid;
+        private List<PathNode> _openList;
+        private List<PathNode> _closedList;
+        private CellData[,] _cells;
+
+        public AStarPathfinder(CellData[,] grid)
+        {
+            _grid = new PathNode[grid.GetLength(0), grid.GetLength(1)];
+            _cells = grid;
+        }
+
+        public Vector2Int GetNextCell(Vector2Int start, Vector2Int end)
+        {
+            var path = FindPath(start, end);
+            return new Vector2Int( path[1].X, path[1].Y);
+        }
+        public List<PathNode> FindPath(Vector2Int start, Vector2Int end)
+        {
+            for (int i = 0; i < _grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < _grid.GetLength(1); j++)
+                {
+                    PathNode pathNode = _grid[i, j] = new PathNode(_grid,i, j);
+                    pathNode.GCost = int.MaxValue;
+                    pathNode.Parent = null;
+                    if(!_cells[i, j].Passable) pathNode.Passable = false;
+                }
+            }
+            PathNode startNode = _grid[start.x, start.y];
+            PathNode endNode = _grid[end.x, end.y];
+            _openList = new List<PathNode> { startNode };
+            _closedList = new List<PathNode>();
+
+            startNode.GCost = 0;
+            startNode.HCost = ManhattanDistance(startNode, endNode);
+
+            while (_openList.Count > 0)
+            {
+                PathNode currentNode = GetLowestFCostPathNode(_openList);
+                if (currentNode == endNode)
+                {
+                    return CalculatePath(endNode);
+                }
+                _openList.Remove(currentNode);
+                _closedList.Add(currentNode);
+
+                foreach (var neighbor in GetNeighborList(currentNode))
+                {
+                    if(_closedList.Contains(neighbor)) continue;
+                    if (!neighbor.Passable)
+                    {
+                        _closedList.Add(neighbor);
+                        continue;
+                    }
+                    int possibleGCost = currentNode.GCost + 1;
+                    CellObject containedObject = _cells[neighbor.X, neighbor.Y].ContainedObject;
+                    if (containedObject != null)
+                    {
+                        if (containedObject is WallObject w)
+                        {
+                            possibleGCost += w.HitPoints;
+                        }
+                    }
+                    
+                    if (possibleGCost >= neighbor.GCost) continue;
+                    
+                    neighbor.Parent = currentNode;
+                    neighbor.GCost = possibleGCost;
+                    neighbor.HCost = ManhattanDistance(neighbor, endNode);
+                    if(!_openList.Contains(neighbor)) _openList.Add(neighbor);
+                }
+            }
+
+            return null;
+        }
+
+        private List<PathNode> GetNeighborList(PathNode node)
+        {
+            List<PathNode> neighbors = new List<PathNode>();
+            if (node.X - 1 >= 0)
+            {
+                neighbors.Add(_grid[node.X - 1, node.Y]);
+            }
+
+            if (node.X + 1 < _grid.GetLength(0))
+            {
+                neighbors.Add(_grid[node.X + 1, node.Y]);
+            }
+
+            if (node.Y - 1 >= 0)
+            {
+                neighbors.Add(_grid[node.X, node.Y - 1]);
+            }
+
+            if (node.Y + 1 < _grid.GetLength(1))
+            {
+                neighbors.Add(_grid[node.X, node.Y + 1]);
+            }
+            return neighbors;
+        }
+        private List<PathNode> CalculatePath(PathNode endNode)
+        {
+            List<PathNode> path = new List<PathNode>();
+            path.Add(endNode);
+            PathNode currentNode = endNode;
+            while (currentNode.Parent != null)
+            {
+                path.Add(currentNode.Parent);
+                currentNode = currentNode.Parent;
+            }
+            path.Reverse();
+            return path;
+        }
+        private int ManhattanDistance(PathNode a, PathNode b)
+        {
+            return Mathf.Abs(a.X - b.X) + Mathf.Abs(a.Y - b.Y);
+        }
+
+        private PathNode GetLowestFCostPathNode(List<PathNode> pathNodeList)
+        {
+            PathNode lowestCostNode = pathNodeList[0];
+            foreach (var pathNode in pathNodeList.Where(pathNode => pathNode.FCost < lowestCostNode.FCost))
+            {
+                lowestCostNode = pathNode;
+            }
+            return lowestCostNode;
+        }
+    }
+
+    public class PathNode
+    {
+        private PathNode[,] _grid;
+        public int X;
+        public int Y;
+        public bool Passable = true;
+
+        private int _gCost;
+        public int GCost
+        {
+            get => _gCost;
+            set
+            {
+                _gCost = value;
+                FCost = value + HCost;
+            }
+        }
+        
+        private int _hCost;
+        public int HCost
+        {
+            get => _hCost;
+            set
+            {
+                _hCost = value;
+                FCost = value + HCost;
+            }
+        }
+        public int FCost;
+
+        public PathNode Parent;
+
+        public PathNode(PathNode[,] grid, int x, int y)
+        {
+            _grid = grid;
+            X = x;
+            Y = y;
+        }
+    }
+}
