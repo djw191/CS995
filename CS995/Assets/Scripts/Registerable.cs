75d9fc49d1bba356c7054af1020a932bb7e267aa
Derek Woodard <50028101+djw191@users.noreply.github.com>
2024-12-12 07:38:29 -0600

    -

---
+++ CS995/Assets/Scripts/Registerable.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using UnityEngine;
+
+public class Registerable : MonoBehaviour
+{
+        public string guid;
+
+        private void Awake()
+        {
+                Attrs.Register(gameObject);
+        }
+}
